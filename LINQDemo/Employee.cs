8327bcd4f44975919c77a954c28663ded166395b
Ankit Jagtap <ankit.jagtap50@gmail.com>
2024-06-27 17:29:41 +0530

    Add project files.

---
+++ LINQDemo/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    internal class Employee
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+
+        public string city { get; set; }
+    }
+}
---
+++ LINQDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using LINQDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        List<Employee> emplist = new List<Employee>()
+        { new Employee{Empid=1,Empname="Jack",city="pune"},
+          new Employee{Empid=1,Empname="Ankita",city="Nashik"},
+          new Employee{Empid=1,Empname="Shankari",city="Mumbai"},
+
+        };
+
+        var findEmplyees=(from e in emplist
+                         orderby e.Empname ascending
+                         select e).ToList();
+        foreach (Employee emp in findEmplyees)
+        {
+            Console.WriteLine(emp.Empid);
+            Console.WriteLine(emp.Empname);
+            Console.WriteLine(emp.city);
+        }
+        int[] arr=new int[10] { 1,2,3,4,5,6,7,8,9,10};
+        var evenNos = from a in arr
+                      where a%2 == 0
+                      select a;
+        foreach (int i in evenNos)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
